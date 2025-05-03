462bbb421c62b9b47699ce72305fc790bd9e831b
Michael A L Costa <github@micleal.dev>
2025-05-03 16:27:55 -0300

    feat(frontend): Adiciona react-router para routing das páginas.
    - Adiciona arquivo das páginas base.

---
+++ frontend/src/components/nav.tsx
@@ -0,0 +1,12 @@
+/** Menu de navegação
+ *
+ * Menu de navegação onde ficará os links para as páginas do site.
+ */
+
+export function Nav() {
+  return (
+    <nav>
+      <h1>Nav</h1>
+    </nav>
+  );
+}
---
+++ frontend/src/page/home.tsx
@@ -0,0 +1,10 @@
+/** Página inicial
+ * 
+ * Apresentação do site e uma breve descrição do que ele faz.
+ * Google Maps apresentando as localizações dos locais que emitem documentos.
+ */
+
+export default function HomePage() {
+  return <div>Home</div>;
+}
+
