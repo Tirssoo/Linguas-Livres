dd147a4c372f85223176751ed831d89ab70f5216
Michael A L Costa <github@micleal.dev>
2025-05-27 22:13:24 -0300

    feat: define rotas de tradução

--- core/LinguasLivres/views.py
+++ core/LinguasLivres/views.py
@@ -2,17 +2,24 @@ from django.shortcuts import render
 from django.http import FileResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.core.files.storage import default_storage
-
-
-def home(request):
-    return render(request, 'LinguasLivres/home.html')
+from .translation.processors import processar_e_traduzir_txt
+from .translation.utils import ler_arquivo_txt
+from .translation.services import traduzir_texto
+import os
 
 @csrf_exempt
 def traduzir_view(request):
     if request.method == "POST" and request.FILES.get("arquivo"):
         uploaded_file = request.FILES["arquivo"]
         caminho = default_storage.save(f"uploads/{uploaded_file.name}", uploaded_file)
-        caminho_traduzido = "processar_e_traduzir_txt"(caminho)
+        caminho_traduzido = processar_e_traduzir_txt(caminho)
         return FileResponse(open(caminho_traduzido, "rb"), as_attachment=True)
     
     return render(request, "upload.html")
+
+def processar_e_traduzir_txt(caminho):
+    texto = ler_arquivo_txt(caminho)
+    traduzido = traduzir_texto(texto)
+    nome_base = os.path.basename(caminho)
+    novo_nome = f"traduzido_{nome_base}"
+    
--- core/core/urls.py
+++ core/core/urls.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from LinguasLivres import views
-from LinguasLivres.views import traduzir_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path("traduzir/", traduzir_view),
-    path('traduzir/', views.traduzir_documento),
+    path("traduzir/", views.traduzir_view),
 ]
